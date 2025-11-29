ae42952491b7cdcd2a15164a05f872994f18456c
fish3er <mateuszrybak631@gmail.com>
2025-11-29 14:17:48 +0100

    aktualizacja pracownika

--- server/app/models/employee.py
+++ server/app/models/employee.py
@@ -4,11 +4,13 @@ from app.utils.db import db
 
 class Employee(db.Model):
     __tablename__ = 'employee'
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name  = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True)
-    qr_code = db.Column(db.String(255))  # unikalny kod QR
-    face_encoding = db.Column(db.LargeBinary)  # zakodowana twarz (numpy array)
-    face_image_path = db.Column(db.String(255))  # ścieżka do zdjęcia referencyjnego
+    email = db.Column(db.String(120), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    face_data = db.relationship('FaceCredential', backref='employee', uselist=False, cascade="all, delete-orphan")
+    qr_code   = db.relationship('QRCredential', backref='employee', uselist=False, cascade="all, delete-orphan")
