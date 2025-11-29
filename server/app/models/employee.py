390ee0c1b0a750cb5d57308a7e5f25f2f15b3e59
fish3er <mateuszrybak631@gmail.com>
2025-11-29 13:58:46 +0100

    dodano szkielet dla procowanika

--- server/app/models/employee.py
+++ server/app/models/employee.py
@@ -1,9 +1,9 @@
 # Przechowuj info pracownika + ścieżkę do zdjęcia referencyjnego
 from datetime import datetime
-from server.app.utils import db
-
+from app.utils.db import db  
 
 class Employee(db.Model):
+    __tablename__ = 'employee'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name  = db.Column(db.String(100), nullable=False)
