1ce3f2c466ebd2b6551b2d1ee492e24569ae520f
fish3er <167292998+fish3er@users.noreply.github.com>
2025-11-29 13:11:03 +0100

    Update employee.py

---
+++ server/app/models/employee.py
@@ -0,0 +1,14 @@
+# Przechowuj info pracownika + ścieżkę do zdjęcia referencyjnego
+from datetime import datetime
+from server.app.utils import db
+
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name  = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    qr_code = db.Column(db.String(255))  # unikalny kod QR
+    face_encoding = db.Column(db.LargeBinary)  # zakodowana twarz (numpy array)
+    face_image_path = db.Column(db.String(255))  # ścieżka do zdjęcia referencyjnego
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
